8b156a94eb9f0b25f19b34c1566aca10dea9652b
tiankai <tiankai@chinatelecom.cn>
2024-09-07 11:20:46 +0800

    feat: start input component coding

---
+++ src/components/vi-input/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <input />
+    </div>
+</template>
+
+<script setup lang='ts'>
+
+</script>
+
+<style scoped>
+
+</style>
--- src/views/input/index.vue
+++ src/views/input/index.vue
@@ -1,12 +1,12 @@
 <template>
     <div class='input-box'>
         <h1>vi-input</h1>
-
+        <vi-input />
     </div>
 </template>
 
 <script setup lang='ts'>
-
+import viInput from '@/components/vi-input/index.vue'
 </script>
 
 <style scoped>
