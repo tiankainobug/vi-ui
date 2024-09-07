9054d1641751e4dded5589ad214b19c813ffbc5f
tiankai <tiankai@chinatelecom.cn>
2024-09-07 11:12:12 +0800

    feat: 初始化项目代码

---
+++ src/views/index.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class='container'>
+        <div @click='goComponent'>Input 输入框</div>
+        <div>Button</div>
+    </div>
+</template>
+
+<script setup lang='ts'>
+
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+const goComponent = () => {
+    router.push('/input')
+}
+</script>
+
+<style scoped>
+.container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    div {
+        width: 200px;
+        height: 50px;
+        background-color: darkcyan;
+        border-radius: 10px;
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        margin-top: 10px;
+        cursor: pointer;
+    }
+    div:hover {
+        background-color: seagreen;
+    }
+}
+</style>
---
+++ src/views/input/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class='input-box'>
+        <h1>vi-input</h1>
+
+    </div>
+</template>
+
+<script setup lang='ts'>
+
+</script>
+
+<style scoped>
+
+</style>
