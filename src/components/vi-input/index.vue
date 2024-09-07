89066dcd63b1fdca16ffa25a65560435dee76637
tiankai <tiankai@chinatelecom.cn>
2024-09-07 16:46:38 +0800

    feat: 1.add scss;2.add common css;3.input component develop

--- src/components/vi-input/index.vue
+++ src/components/vi-input/index.vue
@@ -1,13 +1,39 @@
 <template>
     <div>
-        <input />
+        <input
+            class='vi-input'
+            v-model='model'
+            :disabled='props.disabled ?? false'
+            :maxlength='props.maxLength'
+            :minlength='props.minlength'
+            :placeholder='props.placeholder'
+        />
     </div>
 </template>
 
 <script setup lang='ts'>
+const model = defineModel()
+const props = defineProps<{
+    disabled?: boolean;
+    maxLength?: string | number;
+    minlength?: string | number;
+    placeholder?: string
+}>();
 
 </script>
 
-<style scoped>
+<style scoped lang='scss'>
+@import "styles/variables";
 
+input {
+    width: $input-width;
+    height: $input-height;
+    box-sizing: border-box;
+    border: 1px solid #dcdfe6;
+    border-radius: 5px;
+    padding: 0 10px;
+}
+input:focus {
+    box-shadow: 0 0 0 1px $input-focus-border-color inset;
+}
 </style>
--- src/views/input/index.vue
+++ src/views/input/index.vue
@@ -1,14 +1,30 @@
 <template>
     <div class='input-box'>
         <h1>vi-input</h1>
-        <vi-input />
+        <div class='demo-card'>
+            <h2>正常用法</h2>
+            <vi-input
+                v-model='data.inputValue'
+                :disabled='true'
+            />
+        </div>
+
     </div>
 </template>
 
 <script setup lang='ts'>
 import viInput from '@/components/vi-input/index.vue'
+import { reactive } from 'vue'
+
+const data =reactive({
+    inputValue: ''
+})
 </script>
 
 <style scoped>
-
+.demo-card {
+    width: 400px;
+    border-radius: 10px;
+    padding: 10px;
+}
 </style>
